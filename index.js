ff942c91a2e5e38266d287619b7b743f1153e8fd
Thalia Rodriguez <thaliarpuron@gmail.com>
2020-09-26 00:14:08 -0400

    view employees

--- index.js
+++ index.js
@@ -15,18 +15,76 @@ var connection = mysql.createConnection({
 
   // Your password
   password: "root",
-  database: "EMPLOYEE_TRACKER"
+  database: "employee_tracker"
 });
 
-
-
-
-
-
-
-
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
-  connection.end();
+  start();
 });
+
+
+function start() {
+  const options = [
+    "View all employees",
+    "View all employees by role",
+    "View all employees by manager",
+    "Add employee",
+
+    //"Add department",
+    //"Update employee role.",
+    //"Remove employee",
+    //"Exit.",
+  ];
+  inquirer.prompt([
+    {
+      type: "list",
+      message: "What would you like to do?",
+      choices: options,
+      name: "choice"
+    }
+  ]).then(response => {
+    if (response.choice == options[0]) {
+      allEmployees();
+    } if (response.choice == options[1]) {
+      employeesByRole();
+    } if (response.choice == options[2]) {
+      employeesByManager();
+  });
+};
+
+function allEmployees() {
+  connection.query("SELECT * employee_tracker.employee;", function (error, result) {
+    if (error, result) {
+      console.log(error);
+    }
+    console.table(result);
+    start();
+  });
+}
+
+function employeesByRole() {
+  connection.query("SELECT employee.first_name,employee.last_name,role.title FROM employee INNER JOIN role ON role.id=employee.role_id;",
+    function (error, result) {
+      if (error) {
+        console.log(error);
+      }
+      console.table(result);
+      start();
+    });
+}
+
+function employeesByManager() {
+  connection.query("SELECT * FROM employee WHERE manager_id=1", function (error, result) {
+    if (error, result) {
+      console.log(error);
+    }
+    console.table(result);
+    start();
+  });
+}
+
+
+
+
--- seeds.sql
+++ seeds.sql
@@ -4,16 +4,13 @@ INSERT INTO department (name) VALUES ("Management");
 INSERT INTO department (name) VALUES ("Call Center");
 INSERT INTO department (name) VALUES ("Referrals");
 INSERT INTO department (name) VALUES ("Accounting");
-INSERT INTO department (name) VALUES ("Human Resources");
 
-INSERT INTO role (title, salary, department_id) VALUES ("CEO", 100000, 1);
+INSERT INTO role (title, salary, department_id) VALUES ("Manager", 100000, 1);
 INSERT INTO role (title, salary, department_id) VALUES ("Call Center Agent",25000, 2);
 INSERT INTO role (title, salary, department_id) VALUES ("Referral Coordinator", 25000, 3);
 INSERT INTO role (title, salary, department_id) VALUES ("Accountant", 50000, 4);
-INSERT INTO role (title, salary, department_id) VALUES ("HR Manager", 35000, 5);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Marlow", "Hernandez", 1, 1);
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Thalia", "Rodriguez", 2, NULL);
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Cherilyn", "Smith", 3, NULL);
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Alex", "Ramos", 4, NULL);
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Maria", "Soto", 5, NULL);
